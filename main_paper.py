2d53eaf8d0188e1a8cfdaff49f88a0a29f0a1b9e
failedcoder12 <divyanshverma12@gmail.com>
2019-11-10 18:16:34 +0530

    Final

--- main_paper.py
+++ main_paper.py
@@ -1,5 +1,6 @@
 import partitioning_paper as Partioning
 import rangequery_paper as RangeQuery
+import datetime
 
 print("Creating Database Name dds")
 Partioning.createDB()
@@ -8,14 +9,26 @@ print("Getting connection from dds database")
 con = Partioning.getOpenConnection()
 
 print("Creating and Loading the ratings table")
-Partioning.loadRatings('ratings','data/IJdata.dat',con)
+Partioning.loadRatings('ratings','data/ij.dat',con)
 
 print("Doing Range Partitioning")
 Partioning.rangePartition('ratings',53,con)
 
-print("Performing Range Query")
-RangeQuery.RangeQuery('ratings','aa','az',con)
+st = datetime.datetime.now()
+# print("Performing Range Query")
+RangeQuery.FastRangeQuery('ratings','aa','az',10,con)
+# RangeQuery.RangeQuery('ratings','aa','az',con)
+en = datetime.datetime.now()
 
+print((en-st)*1000)
+
+st = datetime.datetime.now()
+# print("Performing Fast Range Query")
+RangeQuery.RangeQuery('ratings','aa','az',10,con)
+# RangeQuery.RangeQuery('ratings','aa','az',con)
+en = datetime.datetime.now()
+
+print((en-st)*1000)
 
 print("Deleting all Tables")
 Partioning.deleteTables('all',con)
--- rangequery_paper.py
+++ rangequery_paper.py
@@ -2,9 +2,32 @@ import psycopg2
 import os
 import sys
 
-def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection):
-    data = []
+PRIME = 53
+def hash_string(id):
+    num = 0
+    for i in id:
+        num = (num*PRIME + ord(i))%PRIME
+    return num
 
+def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, keylength, openconnection):
+    data = []
+    
+    hash_got = {}
+    list_req = [chr(c+ord('a')) for c in range(0,26)]    
+    heapq.heapify(list_req)
+    
+    while(len(list_req) > 0):
+        st = heapq.heappop(list_req)
+        
+        if(len(st) < keylength):
+            hash_got[hash_string(st)] = 1
+            for c in range(0,26):
+                st = st + chr(c + ord('a'))
+                heapq.heappush(list_req,st)
+        
+        if(len(hash_got) >= 26):
+            break
+    
     cur = openconnection.cursor()
     cur.execute('select current_database()')
     db_name = cur.fetchall()[0][0]
@@ -23,6 +46,45 @@ def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection)
     
     cur.close()
 
+    fh = open("RangeQueryOut.txt","w")
+    fh.write("\n".join(data))
+    fh.close()
+
+import heapq
+
+def FastRangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, keylength, openconnection):
+    data = []
+    cur = openconnection.cursor()
+
+    hash_got = {}
+    list_req = [chr(c+ord('a')) for c in range(0,26)]    
+    heapq.heapify(list_req)
+    
+    while(len(list_req) > 0):
+        st = heapq.heappop(list_req)
+        
+        if(len(st) < keylength):
+            hash_got[hash_string(st)] = 1
+            for c in range(0,26):
+                st = st + chr(c + ord('a'))
+                heapq.heappush(list_req,st)
+        
+        if(len(hash_got) >= 26):
+            break
+        
+    
+    for table in hash_got:
+        table_name = name = "RangeRatingsPart" + str(table)
+        cur.execute("select userid, rating from " + table_name + " where userid >= '" + ratingMinValue + "' and userid <= '" + ratingMaxValue + "'")
+        matches = cur.fetchall()
+
+        for match in matches:
+            partition = table_name
+            data.append(str(partition) + "," + str(match[0]) + "," + str(match[1]))
+    
+    cur = openconnection.cursor()
+
+    cur.close()
     fh = open("RangeQueryOut.txt","w")
     fh.write("\n".join(data))
     fh.close()
